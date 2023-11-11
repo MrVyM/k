54da73348b5e3cb559ac6f3e239285e198f64425
MrVym <la.regle.du.jeu.ytb@gmail.com>
2023-11-11 19:47:43 +0100

    [IO] Document the function on io.h

--- k/io.h
+++ k/io.h
@@ -3,11 +3,16 @@
 
 #include <k/types.h>
 
+// outb transfers a data byte (8 bits) from the register 
+// u16 port
+// u8 val
 static inline void outb(u16 port, u8 val)
 {
 	asm volatile ("outb %0, %1" : /* No output */ : "a"(val), "d"(port));
 }
 
+// inb return a data byte (8 bits) from the register 
+// u16 port
 static inline u8 inb(u16 port)
 {
 	u8 res;
@@ -17,11 +22,16 @@ static inline u8 inb(u16 port)
 	return res;
 }
 
+// outw transfers a data word (16 bits) from the register 
+// u16 port
+// u8 val
 static inline void outw(u16 port, u16 val)
 {
 	asm volatile ("outw %0, %1" : /* No output */ : "a"(val), "d"(port));
 }
 
+// inw return a data word (16 bits) from the register 
+// u16 port
 static inline u16 inw(u16 port)
 {
 	u16 res;
